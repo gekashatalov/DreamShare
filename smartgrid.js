47af8d8b2133c39f7c6414003743d232782debf2
unknown <geka.shatalov@gmail.com>
2020-04-12 18:36:35 +0300

    second

---
+++ smartgrid.js
@@ -0,0 +1,26 @@
+module.exports = {
+    outputStyle: 'less',
+    columns: 24,
+    offset : '10px',
+    mobileFirst: false,
+    container: {
+        maxWidth: "950px",
+        fields: "30px",
+    },
+    breakPoints: {
+        md: {
+            width: "920px",
+            fields: "15px"
+        },
+        sm: {
+            width: "720px"
+        },
+        xs: {
+            width: "576px"
+        },
+        xxs: {
+            width: "420px"
+        }
+    }
+
+};
